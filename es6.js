e841968f4c9856870bf2bf1ccd40fbe4f580f2d7
Dmitriy <developer085@gmail.com>
2016-07-23 18:22:55 +0300

    Update es6.js

--- es6.js
+++ es6.js
@@ -1,2 +1,18 @@
-console.log()
-consol.log('hello')
+//  --- GENERATORS ---
+
+// 1. quiz generator
+function* quiz(count=5){
+   let results = {};
+   let i = 0;
+   while (++i && count >= i ) {
+     results[`Answer #${i}`] = prompt(`Question #${i}`);
+     yield results
+   }
+}
+let quizGenerator = quiz(10);
+
+// var results = quizGenerator.next().value; // ...
+// or call all synchronously, like:
+for (var results of quizGenerator ) {}
+
+console.info("results\n", results)
