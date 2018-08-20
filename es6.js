0d00ab3a9209931418b0209016877647b92c57de
Dmitriy <developer085@gmail.com>
2018-08-20 03:04:29 +0300

    ObjectIterator

--- es6.js
+++ es6.js
@@ -19,3 +19,57 @@ var results = quizGenerator.next('Your question').value; // ...
 //for (var results of quizGenerator ) {}
 
 console.info("results\n", results)
+
+
+
+
+
+
+
+
+class ObjectIterator {
+	
+	constructor (params) {
+		const obj = {...params};
+		const items = Object.keys(obj)
+			.map(key => ({key,value: obj[key]}));
+		const arr = [...items];
+		this.next = this.next.bind(this,arr);
+		this.return = this.return.bind(this,arr);
+		this.throw = this.throw.bind(this,arr);
+		this.restart = this.restart.bind(this,arr,items);
+	}
+
+	[Symbol.iterator] () {return this;}
+
+	next (arr) {
+		const done = !arr.length; // have to be first;
+		const value = arr.shift();
+		return {value,done}
+	}
+
+	throw (arr) {}
+
+	return (arr) {}
+
+	restart (arr,items) {
+		arr.splice(0);
+		arr.push(...items);
+		return this;
+	}
+
+}
+
+function makeIterable (obj) {
+   obj[Symbol.iterator] = function () { return new ObjectIterator(obj) }
+   return obj;
+}
+
+// usage
+const o = {
+	foo:2,
+	bar:3,
+};
+console.log([...o]);
+makeIterable(obj);
+console.log([...o]);
