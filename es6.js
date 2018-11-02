2ec218fa4ae54c5ad42910c2826c6a31d9463158
Dmitriy <developer085@gmail.com>
2018-11-02 03:03:09 +0300

    AsyncQueue

--- es6.js
+++ es6.js
@@ -71,3 +71,108 @@ const o = {
 // console.log([...o]);
 makeIterable(o);
 console.log([...o]);
+
+
+
+
+/**
+ * @description This is async queue automatic handler;
+ * @example 
+ *  const queue = new AsyncQueue();
+ *  queue.subscribe( it => console.log(it));
+ *  queue.push(1,2,3,4,5);
+ */
+class AsyncQueue extends Array {
+  /**
+   *
+   * @param options Object
+   *  - [handleBy] Number : count to handle tasks by.
+   *    - 0 is not aloud;
+   *    - if negative will handle by all tasks in one tick;
+   */
+  constructor (options) {
+    super();
+
+    const { handleBy } = AsyncQueue.normalizeOptions(options);
+
+    Object.defineProperty(this, 'stateKey', {
+      value: Symbol('privateState'),
+      enumerable: false,
+      configurable: false,
+    });
+
+    this[this.stateKey] = {
+      running: true,
+      subscriptions: [],
+      handleBy,
+    };
+
+    this.checkQueue = this.checkQueue.bind(this);
+
+    this.start();
+  }
+
+  start () {
+    this[this.stateKey].running = true;
+    this.tick(this.checkQueue);
+  }
+
+  stop () {
+    this[this.stateKey].running = false;
+  }
+
+  subscribe (callback) {
+    this[this.stateKey].subscriptions.push(callback);
+    return () => this.unsubscribe(callback);
+  }
+
+  unsubscribe (callback) {
+    const index = this[this.stateKey].subscriptions.indexOf(callback);
+    if (index > -1) {
+      this[this.stateKey].subscriptions.splice(index, 1);
+    }
+  }
+
+  checkQueue () {
+    if (!this[this.stateKey].running) {
+      return false;
+    }
+
+    if (this.length > 0) {
+      this.splice(0, this[this.stateKey].handleBy > 0 ? this[this.stateKey].handleBy : this.length)
+        .forEach(task => {
+          this[this.stateKey].subscriptions.forEach(handler => {
+            if (typeof handler === 'function') {
+              handler(task);
+            }
+          });
+        });
+    }
+
+    if (!this[this.stateKey].running) {
+      return false;
+    }
+
+    this.tick(this.checkQueue);
+  };
+
+  tick (callback) {
+    return requestAnimationFrame(callback);
+  }
+
+  static get defaults () {
+    return {
+      handleBy: 1,
+    };
+  }
+
+  static normalizeOptions (options = {}) {
+    options.handleBy = options.handleBy === 0 ? 1 : options.handleBy;
+
+    return Object.assign(
+      this.defaults,
+      options,
+    );
+  }
+}
+
